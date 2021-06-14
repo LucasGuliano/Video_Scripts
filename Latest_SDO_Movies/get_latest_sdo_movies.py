9788c8e30915b7a7faa06ace507a1cd63f5a153d
LucasGuliano <52748594+LucasGuliano@users.noreply.github.com>
2021-06-14 18:28:38 -0400

    Add files via upload

---
+++ Latest_SDO_Movies/get_latest_sdo_movies.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 17:59:47 2021
+
+@author: lguliano
+"""
+
+import wget
+
+wget.download('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0094.mp4')
+wget.download('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0304.mp4')
+wget.download('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0193.mp4')
+wget.download('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_0171.mp4')
+wget.download('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_1600.mp4')
+wget.download('https://sdo.gsfc.nasa.gov/assets/img/latest/latest_1024_fade.mp4')
+
